ccf4dda104a8d42d81a12dd7249064e6783b396d
VadimAspirin <aspirine@protonmail.ch>
2017-12-12 12:03:38 +0500

    Update Main.java

--- chat/client/Main.java
+++ chat/client/Main.java
@@ -1,7 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
+	
+	private static Logger log = Logger.getLogger(Client.class.getName());
+	
+	private static void InitLoader(){
+		try {
+		    LogManager.getLogManager().readConfiguration(
+			    ChatServer.class.getResourceAsStream("/logging.properties"));
+		} catch (IOException e) {
+		    log.log(Level.SEVERE, "Не удалось создать лог. ", e);
+		}
+	    }
+	
 	public static void main(String[] args) {
+		InitLoader();
 		new StartClientWindow();
 	}
 }
